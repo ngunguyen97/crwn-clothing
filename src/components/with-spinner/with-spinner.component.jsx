17b79a72fd918b17a1d761d472297c0691310cb8
lucynguyen <ngu.nguyen@kyanon.digital>
2021-11-28 11:52:53 +0700

    implement code that pulls in our shop data from firestore and stories it into the shop reducer from our shop page. Also included our withSpinner HOC

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...ortherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...ortherProps} />
+    );
+  };
+  return Spinner;
+};
+
+export default WithSpinner;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,14 +1,14 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
-  apiKey: "AIzaSyDJmE_u4iF7rT79LBlDif4iKExEtofMGbM",
-  authDomain: "crwn-db-38842.firebaseapp.com",
-  projectId: "crwn-db-38842",
-  storageBucket: "crwn-db-38842.appspot.com",
-  messagingSenderId: "1073209877724",
-  appId: "1:1073209877724:web:b8d9a8266f4d2f53abc019",
+  apiKey: 'AIzaSyDJmE_u4iF7rT79LBlDif4iKExEtofMGbM',
+  authDomain: 'crwn-db-38842.firebaseapp.com',
+  projectId: 'crwn-db-38842',
+  storageBucket: 'crwn-db-38842.appspot.com',
+  messagingSenderId: '1073209877724',
+  appId: '1:1073209877724:web:b8d9a8266f4d2f53abc019',
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -30,11 +30,41 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (collectionsKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionsKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 firebase.initializeApp(config);
@@ -43,7 +73,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
